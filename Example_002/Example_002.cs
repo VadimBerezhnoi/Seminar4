288c242f767470398b093b7aa530fdccd9b6395a
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-09 21:07:41 +0300

    Написали программу, меняющую знаки элементов массива

---
+++ Example_002/Example_002.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+
+    return result;
+
+} 
+
+void SignChange(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+
+SignChange(array);
+Console.WriteLine(String.Join(", ", array));
