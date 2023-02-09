e3078cbd1577b62a98274e96e9e957abcb796c73
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-09 20:42:37 +0300

    Написана программа по заполнению массива и нахождению суммы положительных и суммы отрицательных элементов

---
+++ Example_001/Example_001.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+
+    return result;
+
+} 
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int el in array)
+{
+    positiveSum += el > 0 ? el : 0; //Простая конструкция оператора if
+    negativeSum += el < 0 ? el : 0; //Простая конструкция оператора if
+}
+
+Console.WriteLine($"Сумма положительных чисел равна {positiveSum}, а сумма отрицательных {negativeSum}");
